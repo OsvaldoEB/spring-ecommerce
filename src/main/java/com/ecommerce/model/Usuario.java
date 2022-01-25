46a1f8d035b112634de5a48505284776c7d44107
OsvaldoEB <osvaldo.jaesba@hotmail.com>
2022-01-25 10:42:30 -0600

    Creando proyecto y entidades

---
+++ src/main/java/com/ecommerce/model/Usuario.java
@@ -0,0 +1,116 @@
+package com.ecommerce.model;
+
+public class Usuario {
+
+    private Long id;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String tipoUsuario; //Cambiar para que se maneje una tabla de tipos de usuario
+    private String contrasena;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nombre, String apellido1, String apellido2, String email, String direccion, String telefono, String tipoUsuario, String contrasena) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.email = email;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.tipoUsuario = tipoUsuario;
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido1='" + apellido1 + '\'' +
+                ", apellido2='" + apellido2 + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", tipoUsuario='" + tipoUsuario + '\'' +
+                ", contrasena='" + contrasena + '\'' +
+                '}';
+    }
+}
