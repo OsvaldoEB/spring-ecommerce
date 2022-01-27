db82cf93ef12f9ac4fe46b924d16bd9baa04afe4
OsvaldoEB <osvaldo.jaesba@hotmail.com>
2022-01-27 10:17:47 -0600

    Implementacion de funcionalidad editar en productos para admin(13)

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -26,18 +29,39 @@ public class ProductoController {
         return "productos/show";
     }
 
+    //Metodo que devuelve la vista para crear un producto
     @GetMapping("/create")
     public String create(){
         return "productos/create";
     }
 
-
+    //Metodo que devuelve la vista para almacenar un producto
     @PostMapping("/save")
     public String save(Producto producto){
         LOGGER.info("Este es el objeto producto {}", producto);
         Usuario u = new Usuario((long) 1, "", "", "", "", "", "", "", "", true);
         producto.setUsuario(u);
         productoService.save(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model){
+        //Creamos un objeto tipo producto
+        Producto producto = new Producto();
+        //Variable Optional para que nos devuelva la busqueda del producto
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+
+        LOGGER.info("Producto buscado: {}",producto);
+        //Se agrega attributo al model para poder cargar los datos obtenidos del producto
+        model.addAttribute("producto", producto);
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
         return "redirect:/productos";
     }
 }
