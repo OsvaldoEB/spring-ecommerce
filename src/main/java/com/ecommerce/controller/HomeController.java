30aa3cb6f285587223391d3140bfa49ce712feec
OsvaldoEB <osvaldo.jaesba@hotmail.com>
2022-01-28 13:05:56 -0600

    añadimos los productos a carrito de compras y los mostramos en la vista carrito (25, 36)

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -54,7 +54,7 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(@RequestParam Long id, @RequestParam Integer cantidad){
+    public String addCart(@RequestParam Long id, @RequestParam Integer cantidad, Model model){
         DetalleOrden detalleOrden = new DetalleOrden();
         Producto producto = new Producto();
         double sumaTotal = 0;
@@ -63,6 +63,25 @@ public class HomeController {
         Optional<Producto> optionalProducto = productoService.get(id);
         log.info("Producto añadido: {}", optionalProducto.get());
         log.info("Cantidad: {}", cantidad);
+        producto = optionalProducto.get();
+
+        //Obtenemos los datos para la vista carrito
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(producto.getPrecio());
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setTotal(producto.getPrecio() * cantidad);
+        detalleOrden.setProducto(producto);
+
+        //Añadimos cada producto (detalleOrden) a la lista
+        detalles.add(detalleOrden);
+
+        //Sumar el total de todos los productos que se encuentren en la lista
+        sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
         return "usuario/carrito";
     }
 
