e30fb982d3c46c857ccc601db1d9b6616837d3a3
OsvaldoEB <osvaldo.jaesba@hotmail.com>
2022-02-02 10:10:12 -0600

    Mostrando la vista compras para el usuario(41)

--- src/main/java/com/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,5 +65,12 @@ public class UsuarioController {
             logger.info("Usuario no existe");
         }
         return "redirect:/";
+    }
+
+    //Metodo para obtener compras
+    @GetMapping("/shopping")
+    public String getShop(Model model, HttpSession session){
+        model.addAttribute("sesion", session.getAttribute("idUsuario"));
+        return "usuario/compras";
     }
 }
