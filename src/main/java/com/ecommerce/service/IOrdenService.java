4daeea22db1cda29c067dd276d545ca193d5d250
OsvaldoEB <osvaldo.jaesba@hotmail.com>
2022-02-02 11:41:27 -0600

    Funcionalidad ver detalle de compra a user (43)

--- src/main/java/com/ecommerce/service/IOrdenService.java
+++ src/main/java/com/ecommerce/service/IOrdenService.java
@@ -4,10 +4,12 @@ import com.ecommerce.model.Orden;
 import com.ecommerce.model.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrdenService {
 
     public List<Orden> findAll();
+    public Optional<Orden> findById(Long id);
     public Orden save (Orden orden);
     public String generarNumeroOrden();
     public List<Orden> findByUsuario(Usuario usuario);
