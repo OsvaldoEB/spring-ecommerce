01d5108b14551014851a348843505a4816494325
OsvaldoEB <osvaldo.jaesba@hotmail.com>
2022-02-02 17:01:24 -0600

    Ver ordenes desde admin (46)

--- src/main/java/com/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/ecommerce/controller/AdministradorController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.controller;
 
 import com.ecommerce.model.Producto;
+import com.ecommerce.service.IOrdenService;
 import com.ecommerce.service.IProductoService;
 import com.ecommerce.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,9 @@ public class AdministradorController {
     @Autowired
     private IUsuarioService usuarioService;
 
+    @Autowired
+    private IOrdenService ordenService;
+
     @GetMapping("")
     public String getHome(Model model){
         List<Producto> productos = productoService.allProductActive();
@@ -31,9 +35,16 @@ public class AdministradorController {
     }
 
     @GetMapping("/usuarios")
-    public String  usuarios(Model model){
+    public String  user(Model model){
         model.addAttribute("usuarios", usuarioService.allUserActive());
         return "administrador/usuarios";
+    }
+
+    @GetMapping("/ordens")
+    public String ordens(Model model){
+        //Añadimos a model loa atributos de la orden así como el metodo findAll para mostrar todas las ordenes
+        model.addAttribute("ordenes", ordenService.findAll());
+        return "administrador/ordenes";
     }
 
 
