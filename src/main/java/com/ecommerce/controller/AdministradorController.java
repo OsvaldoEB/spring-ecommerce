90d5c850e29a2def3c097fd64b854ed36e7c949d
OsvaldoEB <osvaldo.jaesba@hotmail.com>
2022-02-03 09:40:41 -0600

    Funcionalidad para ver los detalles de las ordenes para admin (47)

--- src/main/java/com/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/ecommerce/controller/AdministradorController.java
@@ -1,13 +1,17 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.model.Orden;
 import com.ecommerce.model.Producto;
 import com.ecommerce.service.IOrdenService;
 import com.ecommerce.service.IProductoService;
 import com.ecommerce.service.IUsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +29,8 @@ public class AdministradorController {
 
     @Autowired
     private IOrdenService ordenService;
+
+    private Logger log  = LoggerFactory.getLogger(AdministradorController.class);
 
     @GetMapping("")
     public String getHome(Model model){
@@ -45,6 +51,15 @@ public class AdministradorController {
         //Añadimos a model loa atributos de la orden así como el metodo findAll para mostrar todas las ordenes
         model.addAttribute("ordenes", ordenService.findAll());
         return "administrador/ordenes";
+    }
+
+    @GetMapping("/detailorden/{id}")
+    public String detailOrden(Model model, @PathVariable Long id){
+        log.info("Id de la orden: {}",id);
+        //Se declara un objeto de tipo Orden, como el objeto devuelve un Optional vamos a tener que agregarle .get() al final de la sentencia
+        Orden orden = ordenService.findById(id).get();
+        model.addAttribute("detalles", orden.getDetalle());
+        return "administrador/detalleorden";
     }
 
 
